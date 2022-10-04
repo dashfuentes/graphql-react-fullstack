a40a7c66de1070f6866e699abd7f505e2a0380dc
dashfuentes <smis117112@gmail.com>
2022-10-03 18:32:25 -0600

    switching gears to mongo atlas

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
-mongoose
-  .connect("mongodb://localhost/testdb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((db) => console.log("Db is connected"))
-  .catch((err) => console.log(err));
+const uri = "mongodb+srv://dashfuentes:Bitcoin123@cluster0.zput9.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+} )
+ .then((db) => console.log("Db is connected"))
+   .catch((err) => console.log(err));
